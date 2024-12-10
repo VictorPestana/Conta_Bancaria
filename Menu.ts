47f33a15f6dea6b24a383fe672b63b1f2f2cab03
Victor Pestana <victorpestanavgp@gmail.com>
2024-12-10 12:21:49 -0300

    Menu Banco, Aula dia 11

---
+++ Menu.ts
@@ -0,0 +1,90 @@
+import readlinesync = require("readline-sync");
+import { colors } from "./util/cores";
+
+
+export function main() {
+  let opcao: number;
+
+  while (true) {
+    console.log(colors.bg.black, colors.fg.blue)
+    console.log(
+      "============================================================="
+    );
+    console.log();
+    console.log(
+      " Banco Itáum "
+    );
+    console.log();
+    console.log(
+      "============================================================="
+    );
+    console.log();
+    console.log(" 1 - Criar Conta ");
+    console.log(" 2 - Listar Todas As Contas ");
+    console.log(" 3 - Buscar Conta Por Numero ");
+    console.log(" 4 - Atualizar Dados da Conta ");
+    console.log(" 5 - Apagar Conta ");
+    console.log(" 6 - Sacar ");
+    console.log(" 7 - Depositar ");
+    console.log(" 8 - Transferir Valores Entre Contas ");
+    console.log(" 9 - Sair ");
+    console.log();
+    console.log(
+      "============================================================="
+    );
+    console.log();
+    console.log(colors.reset)
+
+    opcao = readlinesync.questionInt("Escolha uma opcao: ");
+
+    if (opcao === 9) {
+      console.log('\nBanco Itáum\n')
+      sobre();
+      process.exit(0)
+    }
+
+    switch (opcao) {
+      case 1:
+        console.log("Criar Conta");
+        break;
+      case 2:
+        console.log("Listar Todas As Contas");
+        break;
+      case 3:
+        console.log("Consultar dados da Conta - por número");
+        break;
+      case 4:
+        console.log("Atualizar Dados da Conta");
+        break;
+      case 5:
+        console.log("Apagar uma Conta");
+        break;
+      case 6:
+        console.log("Saque");
+        break;
+      case 7:
+        console.log("Depósito");
+        break;
+      case 8:
+        console.log("Transferência Entre Contas");
+        break;
+      case 9:
+        console.log("Saindo do programa...");
+        process.exit(0); 
+        break;
+      default:
+        console.log("Opção inválida.");
+        break;
+    }
+  }
+}
+
+export function sobre(): void {
+  console.log('=========================================')
+  console.log('Projeto Desenvolvido por: ')
+  console.log('Victor Pestana - victorpestanavgp@gmail.com')
+  console.log('https://github.com/VictorPestana')
+  console.log('=========================================')
+}
+
+main();
